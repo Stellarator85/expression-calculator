443f210252bf7ece586226e3392555f689680180
Stellarator85 <86070933+Stellarator85@users.noreply.github.com>
2021-08-29 20:34:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
-    // write your solution here
+function isolate(perem){
+
+    perem = new Function(`return ${perem}`)()
+    
+    if(typeof(perem) === "number") {return perem}
+    
+    else {return 0}
+    
 }
+    
+    
+    function expressionCalculator(expr) {
+    
+        let result = expr.split(' ').join('')
+        let bracketsCount = 0
+        
+        for( i=0; i < result.length; i++){
+        
+            if(result[i] === '(' ) ++bracketsCount;
+            
+            if(result[i] === ')' ) --bracketsCount;
+            
+        }
+        
+       if (bracketsCount != 0) {throw ('ExpressionError: Brackets must be paired!')}
+       
+       if (result.includes('/0')) {throw ('TypeError: Division by zero.')}
+       
+    return isolate(result)
+    }
 
 module.exports = {
     expressionCalculator
